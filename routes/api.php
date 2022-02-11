453be2f9b5169c10d533b3e74e96194b1205802b
Gabriel Cruz <gabrielcruz7400@gmail.com>
2022-02-11 18:43:44 -0300

    changed to only one endpoint

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('deadlift',    'App\Http\Controllers\ApiController@getDeadliftRecord');
-Route::get('blacksquat',  'App\Http\Controllers\ApiController@getBlackSquatRecord');
-Route::get('brenchpress', 'App\Http\Controllers\ApiController@getBrenchPressRecord');
+Route::get('ranking',    'App\Http\Controllers\ApiController@getMovement');
